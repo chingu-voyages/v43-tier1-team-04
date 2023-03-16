a79d29a5a862fc842de4a8035e3b9f62244bfb94
FrnRmn <francescoromandini@gmail.com>
2023-03-16 23:10:40 +0100

    add sport, adventure and occupation stories from drive

---
+++ js/declare_data.js
@@ -0,0 +1,24 @@
+var data = {
+    "Sport": {
+        "Easy": "Once upon a time, there was a (adjective) sports team called the (noun). They had a big game against their (adjective) rivals, the (noun).\
+            The game was (adjective) and both teams showed off their (adjective) skills. In the end, the game ended in a (noun) and both teams showed true (noun) sportsmanship.\
+            It was a game to be remembered for (adjective) years.",
+        "Hard": "It's the championship game, and the tension is [adjective]. The first player up is [name], known for [adjective] speed and [adjective] agility. [Name] sprints down the field and scores a [adjective] goal, leaving the other team in [adjective] shock.\
+             Next up is [name], famous for [adjective] strength and [adjective] endurance. [Name] takes the ball and charges forward, knocking over [adjective] defenders and powering through to score another [adjective] point.\
+             Finally, it's [name]'s turn, known for [adjective] skill and [adjective] finesse. [Name] makes a [adjective] move and sends the ball soaring into the [adjective] net, securing the victory for [pronoun]'s team. The crowd goes wild, and [name] is hailed as a [adjective] hero.\
+             After the game, the players celebrate with [adjective] drinks and [adjective] snacks. They swap stories about their [adjective] victories and [adjective] defeats, and make plans to do it all again next season."
+    },
+    "Adventure": {
+        "Easy": "Once upon a (adjective) day, (name) decided to go on an adventure to find the (adjective) (noun).\
+        (He/She/They) packed a (adjective) backpack with (noun), (noun), and (noun), and set off into the (adjective) wilderness. \
+        As (he/she/they) journeyed through the (adjective) terrain, (name) encountered many challenges, but eventually (he/she/they) reached the (adjective) (noun) and felt a great sense of (emotion).",
+        "Hard": "The adventure begins when [name] decides to explore the [adjective] jungle. Armed with only a [adjective] map and a [adjective] machete, [pronoun] sets out on [adjective] journey into the unknown.\
+        As [name] treks deeper into the jungle, [pronoun] encounters all sorts of [adjective] creatures, from [adjective] monkeys to [adjective] snakes. But [pronoun] refuses to be deterred, and [pronoun] presses on through the [adjective] underbrush.\
+        Finally, after [adjective] hours of hiking, [name] stumbles upon a [adjective] temple hidden deep in the jungle. The temple is guarded by [adjective] warriors, but [name] isn't afraid. [Pronoun] uses [pronoun] wits and [adjective] charm to persuade the warriors to let [pronoun] pass.\
+        Inside the temple, [name] discovers a [adjective] treasure trove of gold and jewels. [Pronoun] fills [pronoun] pockets with [adjective] riches and sets off back through the jungle, victorious. But as [pronoun] makes [pronoun] way home, [pronoun] realizes [pronoun] is being followed by a [adjective] rival adventurer. Will [name] make it back with the treasure intact? Only time will tell!"
+    },
+    "Occupation": {
+        "Easy": "Once upon a time, there was a (adjective) person named (name) who worked as a (occupation). Every day, they would (verb) to work, (adverb) completing their tasks, and (verb) home. \
+        Despite the (adjective) nature of their job, they found fulfillment in their work and took pride in their contributions to society."
+    },
+}
