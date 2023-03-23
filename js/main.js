969457a2deca6fcc3942590fd19a33c333e3b3bf
FrnRmn <francescoromandini@gmail.com>
2023-03-23 18:53:02 +0100

    add category selection and redirection

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-let demo_selected_story = data["Sport"]["Easy"];
+var data = data
+
+// - - - CATEGORY PAGE - - -
+
+// makes category divs clickable, select the category story from data and redirect to input words page
+var categoryClick = function () {
+  let categories = Object.keys(data);
+  for (let category of categories) {
+    document.getElementById(category+"-category").onclick = function () {
+      location.href = "./input_words.html?category="+category;
+    };
+  }
+}
+
+
 
 // - - - INPUT PAGE - - -
+
+// select story based on category parameter found in url
+var selectStory = function () {
+  let url = new URL(window.location.href);
+  let category = url.searchParams.get("category");
+  let selected_story = data[category]["Easy"];
+  return selected_story
+}
 
 // find words sorrounded by round or square brackets from the selected story string: put them into blank_list and substitude them with a * in the original string
 var findBlankWords = function (selected_story) {
@@ -22,7 +44,7 @@ var findBlankWords = function (selected_story) {
 
 // populate html with one input box for each blank word
 var setInputWordsPage = function () {
-  [blank_list, blank_story] = findBlankWords(demo_selected_story);
+  [blank_list, blank_story] = findBlankWords(selectStory());
   input_words_html = ''
   for (let word of blank_list) {
     input_words_html += '<div class="main-content__section-block"><label for="fname">'+word+':</label><input type="text" id="fname" name="fname"><br><br></div>'
